f7d6e62edb33a70e28a56ea01a6a8f4aa3db9316
Kenneth Leine Schulstad <kenneth@leine.cc>
2014-01-12 13:58:49 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Criblog::Application.config.secret_key_base = '1c80ffc12e5dea9b092599519ddb16760548d438321bf197ef5d7dd862e42bf0ffbb9064d860595ff079877fba6882d5cc9948d3c39462e62551300814faad56'
